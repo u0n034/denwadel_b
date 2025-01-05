fca8448f1720bc86d10a1c49edeb2e7c23d225b9
Sayo Uono <sayo.uono@gmail.com>
2025-01-05 16:47:13 +0900

    Implement React Router for path-based routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Header } from './components/Header';
 import { PainPoints } from './components/PainPoints';
 import { Solution } from './components/Solution';
@@ -14,7 +15,7 @@ import { ContactForm } from './components/ContactForm';
 import { CompanyInfo } from './components/CompanyInfo';
 import { Footer } from './components/Footer';
 
-function App() {
+const LandingPageB = () => {
   return (
     <div className="min-h-screen">
       <Header />
@@ -32,6 +33,18 @@ function App() {
       <CompanyInfo />
       <Footer />
     </div>
+  );
+};
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/b" replace />} />
+        <Route path="/b/*" element={<LandingPageB />} />
+        <Route path="/a" element={<div>Landing Page A（準備中）</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
